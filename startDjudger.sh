8520f458b72dbc14e8f176a2b1df25abbf82b9a0
datbeohbbh <phongtomfapp@gmail.com>
2021-03-24 01:02:13 +0700

    update

--- startDjudger.sh
+++ startDjudger.sh
@@ -1,13 +1,6 @@
 #!usr/bin/bash
 
 cd judger
-read -p "did you configured the contest? y / n: " answer 
-
-if [ $answer == "n" ];
-then
-    python3 config_judger.py
-fi
-
 python3 Djudger.py 
 
 ret=$?
